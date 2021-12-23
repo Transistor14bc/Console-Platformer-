d8e2a8120d5e9f7093f0de73046a2d70b151e1f2
TrulikProgramer <95981295+TrulikProgramer@users.noreply.github.com>
2021-12-23 18:49:12 +0200

    Add files via upload

---
+++ GameEngine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace TrulixConsole
+{
+    public class Vector2
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Vector2() : this(0, 0) { }
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class GameObject
+    {
+        public Vector2 position { get; set; }
+    }
+
+    public class Point : GameObject
+    {
+        public Point() : this(0, 0) { }
+        public Point(int x, int y)
+        {
+            position = new Vector2(x, y);
+        }
+    }
+
+    public class Rectangle : GameObject
+    {
+        public int widht { get; set; }
+        public int height { get; set; }
+
+        public Rectangle() : this(0, 0, 0, 0) { }
+        public Rectangle(int x,int y, int width,int height)
+        {
+            position = new Vector2(x, y);
+            this.widht = width;
+            this.height = height;
+        }
+    }
+
+    public static class Physics
+    {
+        public static bool Collision(Point point1, Point point2) // проверка столкновений с точкой
+        {
+            return ((point1.position.x + 1) > point2.position.x) && (point1.position.x < (point2.position.x + 1) &&
+                   (point1.position.y > point2.position.y) && (point1.position.y < point2.position.y));
+        }
+
+        public static bool Collision(Rectangle rect1,Rectangle rect2) // проверка на столкновение квадрата
+        {
+            return ((rect1.position.x + rect1.widht) > rect2.position.x) && (rect1.position.x < (rect2.position.x + rect2.widht)) &&
+                   ((rect1.position.y + rect1.height) > rect2.position.y) && (rect1.position.y < (rect2.position.y + rect2.height));
+        }
+    }
+
+    public static class Camera
+    {
+        public static void Render(Point point)
+        {
+            Console.SetCursorPosition(point.position.x, point.position.y);
+            Console.Write("+");
+        }
+
+        public static void Render(Rectangle rect)
+        {
+            for (int x = 0; x < rect.widht; x++)
+            {
+                for (int y = 0; y < rect.height; y++)
+                {
+                    int xres = x + rect.position.x;
+                    int yres = y + rect.position.y;
+
+                    Console.SetCursorPosition(xres, yres);
+                    Console.Write("@");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using TrulixConsole;
+using System.Threading;
+
+namespace ReliableJump
+{
+    internal class Program
+    {
+        private static Rectangle player = new Rectangle(0,0,5,5);
+        private static Rectangle rect = new Rectangle(0,10,15,5);
+        private static Rectangle rect1 = new Rectangle(17, 8, 15, 5);
+        private static Rectangle rect2 = new Rectangle(34, 14, 15, 5);
+        private static Rectangle rect3 = new Rectangle(55, 14, 15, 5);
+        private static bool run; // переменная запуска
+        private static int gravitySpeed; // переменная скорости гравитацыи
+        private static int speed; // переменая скорости передвижения
+
+        static void Main(string[] args)
+        {
+            run = true;;
+            speed = 1;
+            gravitySpeed = 1;
+
+            Update();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("YOU WIN");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("YOU WIN");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("YOU WIN");
+
+            Console.ReadLine();
+        }
+
+        public static void Update()
+        {
+            while (run)
+            {
+                if(Console.KeyAvailable == true) // если тажата какаято кнопка
+                {
+                    ConsoleKey key = Console.ReadKey().Key; // считываем нажатие кнопки
+
+                    if(key == ConsoleKey.Spacebar) // если нажат пробел
+                    {
+                        gravitySpeed = -5;
+                    }
+                    if(key == ConsoleKey.A) 
+                    {
+                        player.position.x -= speed;
+                    }
+                    if (key == ConsoleKey.D)
+                    {
+                        player.position.x += speed;
+                    }
+                }
+                else // 
+                {
+                    player.position.y += gravitySpeed; // добавляем силу гравитацыи
+                    if (Physics.Collision(rect, player)) {gravitySpeed = 0; }
+                    else if (Physics.Collision(rect1, player)) { gravitySpeed = 0; }
+                    else if (Physics.Collision(rect2, player)) { gravitySpeed = 0; }
+                    else if (Physics.Collision(rect3, player)) { run = false; } // если мы сталкиваемся с 3 квадратом, то цыкл завершаетса
+                    else { gravitySpeed = 1;}
+                }
+
+                Thread.Sleep(3);
+                
+                Console.Clear();
+
+                Camera.Render(rect);
+                Camera.Render(rect1);
+                Camera.Render(rect2);
+                Console.ForegroundColor = ConsoleColor.Green; // устанавливаем зелёный цвет консоли
+                Camera.Render(rect3);
+                Console.ForegroundColor = ConsoleColor.White; // аозращаем белы
+
+                Console.ForegroundColor = ConsoleColor.Red; // устанавлиаем красный
+                Camera.Render(player);
+                Console.ForegroundColor = ConsoleColor.White; // возвращаем белый
+            }
+        }
+    }
+}
